79d42d64725e75c445db0f4c350239791c588fb1
8Mi_Yile <admin@8mi.tech>
2022-12-08 18:47:46 +0800

    先写一些模型，再决定选择哪些代码进插件

---
+++ reference/query_db_table.php
@@ -0,0 +1,19 @@
+<?php
+// 创建函数
+function query_db_table($table_name){
+	// 定义变量
+	$results = array();
+
+	// 检查表是否存在
+	if(YOURLS_DB::table_exists($table_name)){
+		// 获取表的内容
+		$results = YOURLS_DB::get_results("SELECT * FROM $table_name");
+	}
+	
+	// 返回数据
+	return $results;
+}
+
+// 使用函数
+#$blacklist_urls = query_db_table("reviewurl_system_blacklist_url");
+?>
